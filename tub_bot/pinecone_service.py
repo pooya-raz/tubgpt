c2fceb985c29a44956aeebd799d0ecf5b1506161
Christopher Razavian <pooya72@gmail.com>
2023-08-13 14:27:40 +0100

    Move upsert to pinecone_service

--- tub_bot/pinecone_service.py
+++ tub_bot/pinecone_service.py
@@ -1,6 +1,7 @@
 import pinecone
 from Config import Config
 from sentence_transformers import SentenceTransformer
+from diacritic_utils import replace_diacritics
 
 pinecone.init(api_key=Config.api_key, environment=Config.environment)
 index = pinecone.Index("tub")
@@ -10,3 +11,20 @@ def query(query):
     xq = model.encode([query]).tolist()
 
     return index.query(xq, top_k=5, include_metadata=True)
+
+def upsert(texts):
+    embeddings = model.encode(texts)
+
+    pinecone.init(api_key=Config.api_key, environment=Config.environment)
+
+    index = pinecone.Index("tub")
+
+    data = [];
+    for text, embedding in zip(texts, embeddings):
+        id_with_diacritics = text.splitlines()[0]
+        id = replace_diacritics(id_with_diacritics)
+        row = (id, embedding.tolist(),{'text': text})
+        data.append(row)
+
+    print(data)
+    index.upsert(data)
