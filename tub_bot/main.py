65292d8a5ad3d76248760c7002db46fa1a8d02b4
Christopher Razavian <pooya72@gmail.com>
2023-08-13 15:20:05 +0100

    Refactor main while loop

--- tub_bot/main.py
+++ tub_bot/main.py
@@ -1,8 +1,8 @@
 from openai_service import get_response
 from terminal_colors import colors
+
 print(colors.OKCYAN + "Hello, I'm TubBot 🤖. How can I help?" + colors.ENDC)
-quit = False
-while quit == False:
+while True:
     query = input()
     if query == "quit":
         break
