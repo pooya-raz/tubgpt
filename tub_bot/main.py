8b010a675f9741920c7eeb2a827c8e4ac55cc3c2
Christopher Razavian <pooya72@gmail.com>
2023-08-13 15:54:21 +0100

    Add color to response

--- tub_bot/main.py
+++ tub_bot/main.py
@@ -2,11 +2,14 @@ from openai_service import get_response
 import pinecone_service
 from terminal_colors import colors
 
-print(colors.OKCYAN + "Hello, I'm TubBot 🤖. How can I help?" + colors.ENDC)
+def colorText(text):
+    return colors.OKCYAN + text + colors.ENDC
+
+print(colorText("Hello, I'm TubBot 🤖. How can I help?"))
 while True:
     query = input()
     if query == "quit":
         break
     context = pinecone_service.query(query)
     response = get_response(query, context)
-    print(response+"\n")
+    print(colorText(response) + "\n")
